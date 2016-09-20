b2a0a6f0723adbc5673ae327cd0bd877914de851
Sterling Orsten <sgorsten@gmail.com>
2016-09-19 22:50:57 -0700

    Tip #2: Overloads of functions can call one another.
    If the functionality you want is entailed by another function, just go ahead and call it. That way you only have one function to debug, and you get the rest for free. Modern optimizing compilers are pretty good at generating the same thing you would have written by hand anyway.

--- RPCharGen/Dice.cpp
+++ RPCharGen/Dice.cpp
@@ -1,6 +1,6 @@
 #include "Dice.h"
 
-int RollDice(int amount = 1, int sides = 20, int modifier = 0 ) {
+int RollDice(int amount, int sides, int modifier ) {
 
     int r = rand();
     int result;
@@ -26,60 +26,6 @@ int RollDice(int amount = 1, int sides = 20, int modifier = 0 ) {
 
 }
 
-//Overload with no modifier given
-int RollDice(int amount = 1, int sides = 20) {
-
-    int r = rand();
-    int result;
-    int totalRolls = 0;
-
-	static int const max = RAND_MAX / sides*sides;
-
-	//Roll The correct Number of times
-	for (int i = 0; i < amount; i++) {
-
-		
-		while (r >= max) { r = rand(); }
-		int currentRoll = r%sides + 1;
-		//std::cout << "Roll " << "d" << sides << " #" << i + 1 << " = " << currentRoll << std::endl;
-		totalRolls = totalRolls + currentRoll;
-
-	}
-
-	//add the modifier
-	result = totalRolls;
-	return result;
-
-}
-
-//Overload with only the number of sides given
-int RollDice(int sides = 20) {
-
-    int r = rand();
-    int totalRolls = 0;
-
-	//roll a correct sided dice 'amount' times, and apply the modifier
-	static int const max = RAND_MAX / sides*sides;
-
-	while (r >= max) { r = rand(); }
-	int result = r%sides + 1;
-
-	return result;
-}
-
-
-//Overload with no arguments, assumes 1 d20 +0
-int RollDice() {
-
-    int r = rand();
-    int totalRolls = 0;
-
-	int sides = 20;
-	//roll a correct sided dice 'amount' times, and apply the modifier
-	static int const max = RAND_MAX / sides*sides;
-
-	while (r >= max) { r = rand(); }
-	int result = r%sides + 1;
-
-	return result;
-}
+int RollDice(int amount, int sides) { return RollDice(amount, sides, 0); } // If no modifier given, assumed to be zero
+int RollDice(int sides) { return RollDice(1, sides, 0); } // If only sides given, assume one die with no modifier
+int RollDice() { return RollDice(1, 20, 0); } // Overload with no arguments, assumes 1 d20 +0
